424cef5f2ed6e0f577f0749db2e5f030bccbca10
unknown <nivenivethitha2710@gmail.com>
2026-06-29 23:53:22 +0530

    Implemented Task CRUD with priority and due date

--- backend/backend/src/main/java/com/nivethitha/backend/repository/TaskRepository.java
+++ backend/backend/src/main/java/com/nivethitha/backend/repository/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.nivethitha.backend.repository;
 
+import com.nivethitha.backend.entity.Priority;
 import com.nivethitha.backend.entity.Task;
+import com.nivethitha.backend.entity.TaskStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Page<Task> findByStatus(TaskStatus status, Pageable pageable);
+
+    Page<Task> findByPriority(Priority priority, Pageable pageable);
 }
