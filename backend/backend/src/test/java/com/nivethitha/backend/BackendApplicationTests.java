93f03bd5a008dd4daea255e316c44d5df7fb2947
unknown <nivenivethitha2710@gmail.com>
2026-07-01 02:16:22 +0530

    Added backend unit tests

--- backend/backend/src/test/java/com/nivethitha/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/nivethitha/backend/BackendApplicationTests.java
@@ -3,11 +3,24 @@ package com.nivethitha.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void testTaskTitleNotNull() {
+        String title = "MySQL Test";
+        assertNotNull(title);
+    }
+
+    @Test
+    void testTaskStatus() {
+        String status = "PENDING";
+        assertEquals("PENDING", status);
+    }
 }
